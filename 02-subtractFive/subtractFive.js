cc486e2f316423eb80e9d1e1a268dfae9eca93ee
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 00:05:45 -0500

    final commit

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,5 +1,6 @@
 function subtractFive(num) {
-  // Insert code here;
+  let number = num -5 ; //subtracts 5 from the given number
+  return number; //returns the final number
 }
 
 // Do not edit this line;
