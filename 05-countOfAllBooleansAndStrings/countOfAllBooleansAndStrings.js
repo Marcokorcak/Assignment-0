affabeba6f020d0856689fe942198987adc55ab3
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 00:20:38 -0500

    Final commit

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+    let count = 0; //establishes a count vairable
+  for (let i = 0; i<arr.length; i++)	//for loop to check go through the array			
+	{	
+	   if(typeof arr[i] == "boolean" || typeof arr[i] == "string") //checks if the value is a boolean or a string
+	   count++; //increments count if the value is a boolean
+	}
+	return count; //returns the count}
 }
 
 // Do not edit this line;
