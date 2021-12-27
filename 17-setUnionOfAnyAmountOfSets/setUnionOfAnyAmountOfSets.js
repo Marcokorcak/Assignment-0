c523a509cea52ba36c3abc91145bb96cc1f63692
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 14:53:20 -0500

    Final commit

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  let setFinal = new Set;  //creation of a new set that will contain all other sets
+  for(let i = 0; i < args.length; i++)
+	{
+  for(const num of args[i]){   //nested loop that goes through each set, and then each element in the set
+    setFinal.add(num); //adds element to the new set
+			   }
+	}
+return setFinal;  //return the final set which is a joined set
 }
 
 // Do not edit this line;
