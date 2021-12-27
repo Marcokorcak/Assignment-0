68401467b03a9806343a795330bc3bd43b792e04
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 14:50:29 -0500

    Final commit

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,9 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  let productOfNums = 1; //create a variable called productOfNums that is initalized to 1
+  for(let i =0; i < args.length; i ++){
+  productOfNums = productOfNums * args[i];  //goes through the length of the arguments, and multiply each one by each other
+  }
+return productOfNums;  //return the final product after all multiplication
 }
 
 // Do not edit this line;
