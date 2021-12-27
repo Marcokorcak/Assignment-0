ea759a7dad778f8a99433e9bc6313dcf382b40b3
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 00:39:08 -0500

    Final commit

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,11 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let count = 0; //creating count variable
+for (let i = 0; i<nums.length; i++) //for loop to go through the array
+	{	
+	if (nums[i] % 2 == 1) //checks to see if the number is odd
+	count++; //increments counts
+	}
+return count; //return the count
 }
 
 // Do not edit this line;
