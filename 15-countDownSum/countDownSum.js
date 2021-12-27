f4e253e61450009e627cbeb308346ea380c1e19c
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 14:45:35 -0500

    Final commit

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,7 +1,12 @@
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    if (num <=1){  //base case for recursion so the recursion stops when num <= 1
+   return num; //return the final num which is the sum because the base case has been reached
+   }
+   else{
+  return  num + this.countDownSum(num-1);  //recursive code to call the function again and decrement num
   }
+ }
 }
 
 // Do not edit this line;
