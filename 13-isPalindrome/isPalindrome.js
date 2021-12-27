897d0883bc60b7ccbc0bf70793b972b1be760ff9
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 14:36:38 -0500

    Final commit

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,15 @@
 function isPalindrome(word) {
-  // Insert code here;
+  const wordLen = word.length;  //creates a const variable that is equal to the length of the word
+  for(let i = 0; i < wordLen/2; i++)
+  {
+   if(word[i] != word[wordLen-1-i])  //goes through the first half of word and checks if first and second string are equal
+   {
+   return false;  //output false if the word is not a palindrome
+   }  
 }
+return true;  //output true if the word is a palindrome
+}
+
 
 // Do not edit this line;
 module.exports = isPalindrome;
