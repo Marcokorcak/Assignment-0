836c17f9f953e235b62de884bf6a40bd681c1d85
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 00:10:08 -0500

    Final commit

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,5 +1,6 @@
 function cutInHalfAndFloor(num) {
-  // Insert code here;
+  let number = Math.floor(num/2); //divides the number by 2 and if it is a decimal it rounds down and return the floor
+  return number; //return the florr number
 }
 
 // Do not edit this line;
