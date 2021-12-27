bc18d9e5413428a6c641305552adc55fb90905ea
Marco Korcak <marcokorcak02@gmail.com>
2021-12-27 00:16:58 -0500

    Final commit

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,11 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let count = 0; //establishes a count vairable
+  for (let i = 0; i<arr.length; i++)	//for loop to check go through the array			
+	{	
+	   if(typeof arr[i] == "boolean") //checks if the values are boolean
+	   count++; //increments count if the value is a boolean
+	}
+	return count; //returns the count
 }
 
 // Do not edit this line;
